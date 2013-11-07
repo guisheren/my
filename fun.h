f3990631fb0404d63a6f9900f66e2cf0211017df
guisheren <guisheren@gmail.com>
2013-11-07 22:40:15 +0800

    add new .h file

---
+++ fun.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: fun.h
+	> Author: Guisheren
+	> Mail: guisheren@gmail.com 
+	> Created Time: Thu 07 Nov 2013 10:39:24 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
